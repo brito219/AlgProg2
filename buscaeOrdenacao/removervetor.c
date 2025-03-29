3682752f058f2cee9be078321780fd86d7edebe0
João Pedro S. de brito <128265814+brito219@users.noreply.github.com>
2025-03-28 20:14:31 -0400

    new: conteudo de busca e ordenacao

---
+++ buscaeOrdenacao/removervetor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAX 100 
+
+int remove_R(int n, int v[MAX], int k){
+    if (k == n-1){
+        return n-1;
+    }
+    else{
+        v[k] = v[k+1];
+        return remove_R(n,v,k+1);
+    };
+}
+
+int main(){
+    int n, k;
+    int vetor[MAX];
+    printf("tamanho do vetor: \n");
+    scanf("%d", &n);
+
+    printf("valores do vetor de tamanho n: \n");
+    for (int i=0; i<n; i++){
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("indice do que quer remover: \n");
+    scanf("%d", &k);
+
+    n = remove_R(n, vetor, k);
+
+    printf("vetor novo: \n");
+    for (int i =0; i<n; i++){
+        printf("%d ", vetor[i]);
+    }
+
+
+
+
+}
