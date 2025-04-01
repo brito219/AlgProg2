19ca92190c36040b84ea0e4851e12fcbb5ce1297
João Pedro S. de brito <128265814+brito219@users.noreply.github.com>
2025-03-31 20:59:10 -0400

    update: multiplesOf13.c

--- beeCrowd/lista2/multiplesOf13.c
+++ beeCrowd/lista2/multiplesOf13.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+ 
+int main() {
+    
+    int X = 0, Y = 0, soma = 0;
+    scanf("%d", &X);
+    scanf("%d", &Y);
 
-int main()
-{
-
-    /**
-     * Escreva a sua solução aqui
-     * Code your solution here
-     * Escriba su solución aquí
-     */
-
+    if (X > Y) {
+        int aux = X;
+        X = Y;
+        Y = aux;
+    }
+    
+    for (int i = X; i <= Y; i++) {
+        if (i % 13 != 0) {
+            soma = i + soma;
+        }
+    }
+    
+    printf("%d\n", soma);
+ 
     return 0;
 }
